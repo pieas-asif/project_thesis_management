9cc9fbdecb3e176ef56018f32c7a1ce34eabb508
pieas-asif <pieas.asif4@gmail.com>
2019-12-26 13:00:08 +0600

    teacher added, sign up form not fixed

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from .views import HomeView, ProfileView, FormSubmissonView
+from .views import (
+    HomeView,
+    ProfileView,
+    FormSubmissonView,
+    TeacherHomeView,
+    TeacherDetailView,
+    TeacherReviewView
+)
 
 urlpatterns = [
+    path('teacher/review/<int:pk>',
+         TeacherReviewView.as_view(), name='teacher_review'),
+    path('teacher/detail/<int:pk>',
+         TeacherDetailView.as_view(), name='teacher_detail'),
+    path('teacher/', TeacherHomeView.as_view(), name='teacher_home'),
     path('student/create/<int:pk>',
          FormSubmissonView.as_view(), name='create_form'),
     path('student/<int:pk>/', ProfileView.as_view(), name='profile'),
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView
 
 from .models import ProjectModel
@@ -19,3 +19,23 @@ class FormSubmissonView(CreateView):
     model = ProjectModel
     fields = ('title', 'subtitle', 'teacher', 'description',)
     template_name = 'student/create_form.html'
+
+
+class TeacherHomeView(ListView):
+    model = ProjectModel
+    context_object_name = 'projects'
+    template_name = 'teacher/teacher_home.html'
+
+
+class TeacherDetailView(DetailView):
+    model = ProjectModel
+    context_object_name = 'project'
+    template_name = 'teacher/teacher_detail_view.html'
+
+
+class TeacherReviewView(UpdateView):
+    model = ProjectModel
+    context_object_name = 'project'
+    fields = ('first_def_title', 'first_def_time', 'first_def_desc', 'mid_def_title',
+              'mid_def_time', 'mid_def_desc', 'final_def_title', 'final_def_time', 'final_def_desc',)
+    template_name = 'teacher/teacher_review.html'
